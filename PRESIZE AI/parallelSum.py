bcec31bcee343633e857b7d654de2b6ace69394c
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-16 03:00:39 +0530

    trying to solve presize.AI test

---
+++ PRESIZE AI/parallelSum.py
@@ -0,0 +1,13 @@
+def solve(arr):
+    n = len(arr)
+    if(n % 2 != 0):
+        return -1
+    totalSum = sum(arr)
+    if(sum % 2 != 0):
+        return -1
+    arr.sort()
+    for i in range(n):
+        
+
+arr = [1, 2, 3, 4]
+print(solve(arr))
