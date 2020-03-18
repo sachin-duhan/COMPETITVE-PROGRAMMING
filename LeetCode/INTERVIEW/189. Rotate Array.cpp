96cdf46dc13a614b4e12ea64e7904189b0fa934b
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-19 00:36:17 +0530

    adding another O(1) space solution for rotate an array

--- LeetCode/INTERVIEW/189. Rotate Array.cpp
+++ LeetCode/INTERVIEW/189. Rotate Array.cpp
@@ -12,6 +12,26 @@ void rotate(vector<int> &nums, int k)
         nums[i] = v[i];
 }
 
+// void rotate(int[] nums, int k)
+// {
+//     k %= nums.length;
+//     reverse(nums, 0, nums.length - 1);
+//     reverse(nums, 0, k - 1);
+//     reverse(nums, k, nums.length - 1);
+// }
+
+// void reverse(int[] nums, int start, int end)
+// {
+//     while (start < end)
+//     {
+//         int temp = nums[start];
+//         nums[start] = nums[end];
+//         nums[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
+
 int main()
 {
     vector<int> v({1, 2, 3, 4, 5, 6, 7});
