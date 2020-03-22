bb221c79570a0362fa3baf7dbf7a33428fc353d4
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-23 01:57:21 +0530

    adding another solution

--- LeetCode2/238. Product of Array Except Self.cpp
+++ LeetCode2/238. Product of Array Except Self.cpp
@@ -13,6 +13,20 @@ vector<int> productExceptSelf(vector<int> &nums)
     return ans;
 }
 
+vector<int> anotherSolution(vector<int> nums){
+    vector<int> v(nums.size(), 1);
+    for (int i = 1; i < nums.size(); i++)
+    {
+        v[i] = v[i - 1] * nums[i - 1];
+    }
+    int lastNum = nums[nums.size() - 1];
+    for (int i = nums.size() - 2; i >= 0; i--)
+    {
+        v[i] = v[i] * lastNum;
+        lastNum *= nums[i];
+    }
+    return v;
+}
 
 int main()
 {
