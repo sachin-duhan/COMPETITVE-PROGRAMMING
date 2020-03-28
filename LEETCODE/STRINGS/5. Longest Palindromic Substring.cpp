5561d0c1397f8ce054d8589e5a369d11ba8e1d75
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-29 05:06:52 +0530

    finding size of longest palindrome size..

---
+++ LEETCODE/STRINGS/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestPalindrome(string s)
+{
+    
+}
+
+int main()
+{
+    string s = "ababd";
+    cout << longestPalindrome(s) << endl;
+    return 0;
+}
---
+++ LEETCODE/STRINGS/longest palindrome that can be formed.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int size_of_largest_palindrome(string s)
+{
+    int ans = 0;
+    unordered_map<char, int> m;
+    for (int i = 0; i < s.length(); i++)
+        m[s[i]]++;
+    bool isOne = false;
+    for (auto itr = m.begin(); itr != m.end(); itr++)
+    {
+        if (itr->second % 2 == 0)
+            ans += itr->second;
+        if (!isOne && itr->second % 2 != 0)
+        {
+            ans += 1;
+            isOne = true;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    string s = "abccccdd";
+    cout << size_of_largest_palindrome(s) << endl;
+    return 0;
+}
