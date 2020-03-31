59e9ad664abb140dd2231d1fbb2cbb40381483c1
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-03-31 12:33:34 +0530

    update

--- LEETCODE/ARRAY/992. Sort by parity II.cpp
+++ LEETCODE/ARRAY/992. Sort by parity II.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 vector<int> sortArrayByParityII(vector<int> &A)
 {
     int j = 1;
@@ -12,6 +13,7 @@ vector<int> sortArrayByParityII(vector<int> &A)
         }
     return A;
 }
+
 int main()
 {
     vector<int> input = {1, 2, 3, 4, 5, 6}, ans = sortArrayByParityII(input);
