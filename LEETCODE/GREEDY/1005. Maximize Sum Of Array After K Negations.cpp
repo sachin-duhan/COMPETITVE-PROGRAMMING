fb46a974430dd10581f46f24214fe19340f01bde
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-02 16:30:58 +0530

    solved 1005 leetcode

--- LEETCODE/GREEDY/1005. Maximize Sum Of Array After K Negations.cpp
+++ LEETCODE/GREEDY/1005. Maximize Sum Of Array After K Negations.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int largestSumAfterKNegations(vector<int> &A, int K)
 {
-    return K % 2 == 0 ? accumulate(A.begin(), A.end(), 0) : accumulate(A.begin(), A.end(), 0) - *min_element(A.begin(), A.end());
+    if (K != 0)
+        for (int i = 0; i < K; i++)
+        {
+            auto itr = min_element(A.begin(), A.end());
+            *itr = -(*itr);
+        }
+    return accumulate(A.begin(), A.end(), 0);
 }
 
 int main()
 {
     vector<int> val{3, -1, 0, 2};
-    cout << largestSumAfterKNegations(val, 3) << endl;
+    cout << largestSumAfterKNegations(val, 2) << endl;
     return 0;
 }
