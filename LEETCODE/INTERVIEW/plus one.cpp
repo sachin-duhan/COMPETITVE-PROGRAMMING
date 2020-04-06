54d80e6035243cc7c15f489d34d0f9555164aee7
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-06 17:38:00 +0530

    solved plus one interview problem

---
+++ LEETCODE/INTERVIEW/plus one.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> handleExtremeCase(int size)
+{
+    vector<int> ans(size + 1, 0);
+    ans[0] = 1;
+    return ans;
+}
+
+vector<int> plusOne(vector<int> &digits)
+{
+    int i = digits.size() - 1;
+    while (i >= 0)
+    {
+        digits[i]++;
+        digits[i] %= 10;
+        if (i == 0 && digits[i] == 0)
+            return handleExtremeCase(digits.size());
+        if (digits[i] != 0)
+            break;
+        i--;
+    }
+    return digits;
+}
+
+int main()
+{
+    vector<int> num{9,9};
+    plusOne(num);
+    return 0;
+}
