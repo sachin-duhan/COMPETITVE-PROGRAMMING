4c7daa5f4465e5a489aa82144fef949aa6fd90b5
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-07 09:25:45 +0530

    rotate image solved agasin

---
+++ LEETCODE/INTERVIEW/rotate image.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// solution typed in Leetcode IDE!!
+
+// LOGIC
+// TRANSPOSE 
+// REVERSE
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        // taking transpose!!
+        for (int i = 0; i < matrix.size(); i++)
+            for (int j = i; j < matrix[0].size(); j++)
+                swap(matrix[i][j], matrix[j][i]);
+
+        for (int i = 0; i < matrix.size(); i++)
+            reverse(matrix[i].begin(),matrix[i].end());
+    }
+};
