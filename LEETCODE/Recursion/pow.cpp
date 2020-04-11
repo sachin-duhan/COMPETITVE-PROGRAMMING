fb7f8b40224d11489b507b34c4abf7cf47071ac9
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-11 20:38:45 +0530

    wrote my power function

--- LEETCODE/Recursion/pow.cpp
+++ LEETCODE/Recursion/pow.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+float power(float x, int y)
+{
+    float temp;
+    if (y == 0)
+        return 1;
+    temp = power(x, y / 2);
+    if (y % 2 == 0)
+        return temp * temp;
+    else
+    {
+        if (y > 0)
+            return x * temp * temp;
+        else
+            return (temp * temp) / x;
+    }
+}
+
 int main()
 {
-    
+    cout << power(2.0,2) << endl;
     return 0;
 }
