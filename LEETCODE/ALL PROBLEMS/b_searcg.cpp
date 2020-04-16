09c668b2b4f0dfd80a044a45dbedf370832c56b1
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-16 10:34:21 +0530

    binary search

---
+++ LEETCODE/ALL PROBLEMS/b_searcg.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int pivot, left = 0, right = nums.size() - 1;
+        while (left <= right)
+        {
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] == target)
+                return pivot;
+            if (target < nums[pivot])
+                right = pivot - 1;
+            else
+                left = pivot + 1;
+        }
+        return -1;
+    }
+};
