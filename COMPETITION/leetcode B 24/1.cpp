55684186987ef21504e86a834c24f2e522a705c8
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-18 21:39:03 +0530

    update

--- COMPETITION/leetcode B 24/1.cpp
+++ COMPETITION/leetcode B 24/1.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int minStartValue(vector<int> &nums)
+{
+    vector<int> dp(nums.size(), 0);
+    dp[0] = nums[0];
+    for (int i = 1; i < nums.size(); i++)
+    {
+        dp[i] = dp[i - 1] + nums[i];
+    }
+    int ans = *min_element(dp.begin(), dp.end());
+    return ans >= 1 ? 1 : abs(ans) + 1;
+}
+
 int main()
 {
 
