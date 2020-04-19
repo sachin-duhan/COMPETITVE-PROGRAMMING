f83eb73b0c6fcfb095ff21a9f19f8ca7a780dae5
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-20 00:32:35 +0530

    update and solution

--- ALGORITHM/GREEDY/763.cpp
+++ ALGORITHM/GREEDY/763.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 vector<int> partitionLabels(string S)
 {
-    map<char, pair<int, int>> m;
+    vector<int> chars(26, 0);
+    for (int i = 0; i < S.length(); i++)
+        chars[S[i] - 'a'] = i;
+    int j = 0, anchor = 0;
+    vector<int> ans;
     for (int i = 0; i < S.length(); i++)
     {
-        auto itr = m.find(S[i]);
-        if (itr != m.end())
-            m[S[i]].second = i;
-        else
-            m.insert(make_pair(S[i], make_pair(i, i)));
+        j = max(j, chars[S[i] - 'a']);
+        if (i == j)
+        {
+            ans.push_back(i - anchor + 1);
+            anchor = i + 1;
+        }
     }
-    for (auto itr = m.begin(); itr != m.end(); itr++)
-    {
-        
-    }
-    return {0};
+    return ans;
 }
 
 int main()
