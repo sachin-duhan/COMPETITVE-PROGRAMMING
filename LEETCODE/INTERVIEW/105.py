3eef2d2881c12a6267f37e9c7da092f3b2393394
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-21 04:52:48 +0530

    update

---
+++ LEETCODE/INTERVIEW/105.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        i = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:1+i], inorder[:i])
+        root.right = self.buildTree(preorder[1+i:], inorder[i+1:])
+        return root
