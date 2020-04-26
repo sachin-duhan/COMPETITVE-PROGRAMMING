8f12f9c9a8c5a4344b7343d8409ff2509ea07467
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-27 02:57:25 +0530

    update

--- ALGORITHM/LCS/LCS DP.cpp
+++ ALGORITHM/LCS/LCS DP.cpp
@@ -1,13 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(string s, string t){
-    vector<vector<int>> dp(s.length(),vector<int>(t.length(),0));
-    
+/*
+we need a dp for m+1 and n+1
+the first element of the dp represent the empty string!
+row(0) = 0 and col(0) = 0
+each cell in dp[i][j] represent the result of the max LCS of the string s and string t 
+if s[i] == t[j] the dp[i-1][j-1] contains the answer of the LCS without including the s[i] and t[i] char
+so dp[i][j] = dp[i-1][j-1]+1 is the new value
+and when the value doesnot match than in that case we use the max of the dp[i-1][j] and d[i][j-1]
+*/
+
+int LCS(string s, string t){
+    int m =s.length(), n =t.length();
+    vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= n; j++)
+        {
+            if(s[i-1]==t[i-1]) dp[i][j] = dp[i-1][j-1]+1;
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    return dp[m-1][n-1];
 }
 
 int main()
 {
-    string s = "abschs", t = "acjba";
+    string s = "abc", t = "ac";
+    cout << LCS(s,t) << endl;
     return 0;
 }
