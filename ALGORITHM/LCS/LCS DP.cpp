d683d694370ebd4749eed262de9d899798948681
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-27 02:17:50 +0530

    minor update

---
+++ ALGORITHM/LCS/LCS DP.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(string s, string t){
+    vector<vector<int>> dp(s.length(),vector<int>(t.length(),0));
+    
+}
+
+int main()
+{
+    string s = "abschs", t = "acjba";
+    return 0;
+}
--- ALGORITHM/MAIN/longest common subsequence.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    string s = "abschs", t = "acjba";
-return 0;
-}
