2a2677df02df0db56498d24388ad910fa6b49e04
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-01 04:36:11 +0530

    update

--- LEETCODE/ALL PROBLEMS/859.cpp
+++ LEETCODE/ALL PROBLEMS/859.cpp
@@ -23,6 +23,28 @@ bool buddyStrings(string A, string B)
     return true;
 }
 
+class Solution {
+public:
+    bool buddyStrings(string A, string B) {
+        if(A.length()!=B.length()) return false;
+        vector<pair<int,int>>v;bool flag=false;
+        unordered_map<char,int>mp;
+        for(int i=0 ; i<A.length() ; i++)
+        {
+            if(A[i]!=B[i]) v.push_back({A[i],B[i]});
+            else mp[A[i]]++;
+            if(mp[A[i]]>1) flag=true;              //for cases where same character at different position can be swapped example acab and acab.
+        }
+        if(v.size()==2)
+        {
+            if(v[0].first==v[1].second and v[0].second==v[1].first) return true;
+            else return false;
+        }
+        if(v.size()==0 and flag) return true;
+        return false;
+    }
+};
+
 int main()
 {
 
