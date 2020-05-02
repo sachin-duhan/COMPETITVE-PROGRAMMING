1743af32d7de8a4b48bab68344bbf338252c8683
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-03 05:16:55 +0530

    day 2 p 1

---
+++ LEETCODE/MAY CHALLANGE/1.2.cpp
@@ -0,0 +1,16 @@
+#include<map>
+class Solution
+{
+public:
+    int numJewelsInStones(std::string J,std::string S)
+    {
+        std::map<char, int> m;
+        for (char c : J)
+            m[c]++;
+        int ans = 0;
+        for (char c : S)
+            if (m.find(c) != m.end())
+                ans++;
+        return ans;
+    }
+};
