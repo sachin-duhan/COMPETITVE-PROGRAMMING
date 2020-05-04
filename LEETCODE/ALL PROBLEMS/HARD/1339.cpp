bd6cc5afa62c1c3fd99f0b224b92781687ac6469
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-04 05:39:39 +0530

    updating one approach but it  failed in some cases!

---
+++ LEETCODE/ALL PROBLEMS/HARD/1339.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#define mod 1000000007
+
+class Solution
+{
+public:
+    vector<pair<int, int>> sums;
+    int helper(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        int l = helper(root->left);
+        int r = helper(root->right);
+        sums.push_back({l, r + root->val});
+        sums.push_back({l + root->val, r});
+        root->val += l + r;
+        return root->val;
+    }
+
+    vector<int> nodes;
+    void inorder(TreeNode *root)
+    {
+        if (!root)
+            return;
+        inorder(root->left);
+        nodes.push_back(root->val);
+        inorder(root->right);
+    }
+
+    TreeNode *buildTree(vector<int> nodes, int start, int end)
+    {
+        if (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            TreeNode *temp = new TreeNode(nodes[mid]);
+            temp->right = buildTree(nodes, mid + 1, end);
+            temp->left = buildTree(nodes, start, mid - 1);
+            return temp;
+        }
+        return NULL;
+    }
+
+    int maxProduct(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        TreeNode *newNode = root;
+        if (!root->right || !root->left)
+        {
+            inorder(root);
+            newNode = buildTree(nodes, 0, nodes.size() - 1);
+        }
+
+        helper(newNode);
+        set<int, greater<int>> ans;
+        for (auto a : sums)
+            ans.insert(a.first * a.second);
+        return *ans.begin() % mod;
+    }
+};
