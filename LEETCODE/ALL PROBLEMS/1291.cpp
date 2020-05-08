3f41c695e1f5bab221ebbc477c6083271d138ff3
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-08 21:26:17 +0530

    update

---
+++ LEETCODE/ALL PROBLEMS/1291.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> sequentialDigits(int low, int high)
+{
+    vector<int> ans;
+    
+    return ans;
+}
+
+int main()
+{
+    vector<int> output = sequentialDigits(10, 10000);
+    for (auto a : output)
+        cout << a << " ";
+    cout << endl;
+    return 0;
+}
