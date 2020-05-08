1b97c16c9ca9ad9912bdff654ac389e774c0bfe6
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-08 11:03:53 +0530

    update

--- INTERVIEW/Tower hopping problem.cpp
+++ INTERVIEW/Tower hopping problem.cpp
@@ -9,8 +9,28 @@ bool solve(vector<int> arr, int index)
         return false;
     bool flag = false;
     for (int i = arr[index]; i >= 0; i--)
-       if(!flag) flag = solve(arr, index + i);
+        if (!flag)
+            flag = solve(arr, index + i);
     return flag;
+}
+
+int solve2(vector<int> arr)
+{
+    int index = 0;
+    while (index < arr.size())
+    {
+        int new_index = 0;
+        for (int i = 1; i < arr[index]; i++)
+        {
+            if (index + i >= arr.size())
+                return true;
+            else
+                new_index = max(new_index, arr[index + i]);
+        }
+        if(index == 0) return false;
+        else index = new_index;
+    }
+    return true;
 }
 
 int main()
