23b7103cd9c8a2de20019ad8e1300a42d0cf708a
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-17 16:08:29 +0530

    update

--- LEETCODE/ALL PROBLEMS/remove duplicates from sorted array.cpp
+++ LEETCODE/ALL PROBLEMS/remove duplicates from sorted array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int ans = 0;
+    int ans = 1;
     vector<int> input{1, 1, 2, 2, 2, 4, 5, 6, 7};
     for (int i = 1; i < input.size(); i++)
         if (input[i] != input[i - 1])
