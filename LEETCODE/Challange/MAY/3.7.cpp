ca8bf4b6d324933358598f54c4dcf5d59c271eba
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-22 06:35:18 +0530

    starting solution

---
+++ LEETCODE/Challange/MAY/3.7.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void dfs(vector<vector<int>> arr, int i, int j)
+{
+}
+
+int countSquares(vector<vector<int>> &matrix)
+{
+    int ans = 0;
+    int m = arr.size(), n = arr[0].size(), count = min(m, n);
+    // solving here!
+}
+
+int main()
+{
+    vector<vector<int>> matrix{{0, 1, 1, 1}, {1, 1, 1, 1}, {0, 1, 1, 1}};
+    return 0;
+}
