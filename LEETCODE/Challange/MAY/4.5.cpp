1d66e9acc0829cdadbdb2274670e696fdea5ff9a
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-27 09:30:39 +0530

    updating solution

---
+++ LEETCODE/Challange/MAY/4.5.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findMaxLength(vector<int> &nums)
+    {
+        map<int, int> m;
+        m.insert(make_pair(0, -1));
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+                count++;
+            else
+                count--;
+            if (m.find(count) != m.end())
+                maxlen = max(maxlen, i - m[count]);
+            else
+                m.insert(make_pair(count, i));
+        }
+        return maxlen;
+    }
+};
+
+int main()
+{
+    Solution temp;
+    vector<int> input{0, 1, 0};
+    cout << temp.findMaxLength(input) << endl;
+    return 0;
+}
