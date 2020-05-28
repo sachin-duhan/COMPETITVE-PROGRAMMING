e1982fe284a0be0d130f35faa209913189c466c4
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-29 03:05:21 +0530

    adding another BS solution

--- LEETCODE/ALL PROBLEMS/153.cpp
+++ LEETCODE/ALL PROBLEMS/153.cpp
@@ -1,19 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+int findMin(vector<int> &nums)
 {
-public:
-    int findMin(vector<int> &nums)
+    return *min_element(nums.begin(), nums.end());
+}
+
+int findMin_using_binary_search(vector<int> &nums)
+{
+    int lo = 0, hi = nums.size() - 1;
+    while (lo < hi)
     {
-        return *min_element(nums.begin(), nums.end());
+        int mid = lo + (hi - lo) / 2;
+        if (nums[mid] < nums[hi])
+        {
+            hi = mid;
+        }
+        else
+        {
+            lo = mid + 1;
+        }
     }
-};
+    return nums[lo];
+}
 
 int main()
 {
     vector<int> input{1, 2, 3, 4, 5};
-    Solution solve;
-    cout << solve.findMin(input) << endl;
+    cout << findMin(input) << endl;
     return 0;
 }
