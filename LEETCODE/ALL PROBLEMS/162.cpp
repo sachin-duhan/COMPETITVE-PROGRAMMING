ff40d8efcc71948dfa59538419f4f0b7fe1a70dd
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-29 02:03:32 +0530

    drive code update

--- LEETCODE/ALL PROBLEMS/162.cpp
+++ LEETCODE/ALL PROBLEMS/162.cpp
@@ -14,3 +14,11 @@ public:
         return nums.size() - 1;
     }
 };
+
+int main()
+{
+    vector<int> input{1, 2, 3, 4, 5, 6, 6, 8, 4};
+    Solution solve;
+    cout << solve.findPeakElement(input) << endl;
+    return 0;
+}
