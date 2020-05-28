04cad054d5b73c409f3394ab24a37a047deb98e4
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-29 02:57:49 +0530

    solved question

--- LEETCODE/ALL PROBLEMS/153.cpp
+++ LEETCODE/ALL PROBLEMS/153.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        return *min_element(nums.begin(), nums.end());
+    }
+};
+
 int main()
 {
     vector<int> input{1, 2, 3, 4, 5};
-    
+    Solution solve;
+    cout << solve.findMin(input) << endl;
     return 0;
 }
