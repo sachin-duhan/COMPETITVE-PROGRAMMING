da2fb06449bccd86b1333736f311581de81af07c
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-29 00:24:32 +0530

    update

--- LEETCODE/ALL PROBLEMS/HARD/403. frog jumps.cpp
+++ LEETCODE/ALL PROBLEMS/HARD/403. frog jumps.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
@@ -27,3 +27,26 @@ public:
         return false;
     }
 };
+
+/*JAVA SOLUTION FOR THE SAME!*/
+
+// class Solution {
+//     public boolean canCross(int[] stones) {
+//         int N = stones.length;
+//         boolean[][] dp = new boolean[N][N + 1];
+//         dp[0][1] = true;
+
+//         for(int i = 1; i < N; ++i){
+//             for(int j = 0; j < i; ++j){
+//                 int diff = stones[i] - stones[j];
+//                 if(diff < 0 || diff > N || !dp[j][diff]) continue;
+//                 dp[i][diff] = true;
+//                 if(diff - 1 >= 0) dp[i][diff - 1] = true;
+//                 if(diff + 1 <= N) dp[i][diff + 1] = true;
+//                 if(i == N - 1) return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
