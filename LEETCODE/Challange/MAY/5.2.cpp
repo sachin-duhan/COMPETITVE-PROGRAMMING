0b24c2bc00e27355a54f6aa9e85d4923df4ff785
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-05-31 05:11:26 +0530

    adding finally accepted solution

--- LEETCODE/Challange/MAY/5.2.cpp
+++ LEETCODE/Challange/MAY/5.2.cpp
@@ -5,32 +5,30 @@ vector<vector<int>> kClosest(vector<vector<int>> &points, int K)
 {
     if (K == points.size())
         return points;
-    map<float, vector<int>> m;
+    map<float, vector<vector<int>>> m;
     for (auto arr : points)
     {
-        
-        int distance = sqrt(arr[0] * arr[0] + arr[1] * arr[1]);
-        m[distance] = arr;
+        float distance = sqrt(arr[0] * arr[0] + arr[1] * arr[1]);
+        m[distance].push_back(arr);
     }
-    for (auto val : m)
-        cout << val.first << endl;
     vector<vector<int>> ans;
     for (auto itr = m.begin(); itr != m.end(); itr++)
     {
         if (K <= 0)
             break;
         else
-        {
-            ans.push_back(itr->second);
-            K--;
-        }
+            for (auto c : itr->second)
+            {
+                ans.push_back(c);
+                K--;
+            }
     }
     return ans;
 }
 
 int main()
 {
-    vector<vector<int>> points{{1, 3}, {-2, 2}, {3, 3}};
+    vector<vector<int>> points{{1, 1}, {-2, 2}, {3, 3}};
     kClosest(points, 2);
     return 0;
 }
