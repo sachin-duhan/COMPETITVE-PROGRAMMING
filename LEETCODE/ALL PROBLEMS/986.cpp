e11f2c3e51f3d29982841fc5091be4cc20f89540
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-03 00:08:09 +0530

    trying some solution

---
+++ LEETCODE/ALL PROBLEMS/986.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> intervalIntersection(vector<vector<int>> &A, vector<vector<int>> &B)
+{
+    int i = 0, j = 0, A_size = A.size(), B_size = B.size();
+    vector<vector<int>> ans;
+    if (!A_size || !B_size)
+        return ans;
+    while (i < A_size && j < B_size)
+    {
+        if (A[i][1] < B[j][0])
+            i++;
+        else if (A[i][0] > B[j][1])
+            j++;
+        else
+        {
+            ans.push_back({max(A[i][0], B[j][0]), min(A[i][1], B[j][1])});
+            if (i <= j)
+                i++;
+            else
+                j++;
+        }
+    }
+    return ans;
+}
