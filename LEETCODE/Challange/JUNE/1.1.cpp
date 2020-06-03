e87f4668508bd109e4735f1239668c08db3c2c9f
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-04 00:46:20 +0530

    trying 1.1.

---
+++ LEETCODE/Challange/JUNE/1.1.cpp
@@ -0,0 +1,17 @@
+// invert binary tree
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *invert(TreeNode *root)
+{
+}
