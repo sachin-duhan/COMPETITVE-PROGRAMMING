de49b49dff5fd9e2c57c4e096460f42afc0074a0
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-04 00:44:02 +0530

    solved 1.2

---
+++ LEETCODE/Challange/JUNE/1.2.cpp
@@ -0,0 +1,18 @@
+
+//Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
