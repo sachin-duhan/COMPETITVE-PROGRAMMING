c60bd49c853fdc2d3fb0ce9e13183d5f4557ac1e
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-05 11:45:59 +0530

    updating final solution

--- LEETCODE/ALL PROBLEMS/43.cpp
+++ LEETCODE/ALL PROBLEMS/43.cpp
@@ -31,6 +31,38 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    string multiply(string num1, string num2)
+    {
+        if (num1 == "0" || num2 == "0")
+            return "0";
+        if (num1 == "1")
+            return num2;
+        if (num2 == "1")
+            return num1;
+        int n = num1.length(), m = num2.length();
+        vector<int> result(n + m, 0);
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                int sum = (num2[i] - '0') * (num1[j] - '0') + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+        string res;
+        for (int num : result)
+        {
+            if (res.empty() && num == 0)
+                continue; //Remove Leading Zeros
+            res += to_string(num);
+        }
+        return res;
+    }
+};
 int main()
 {
 }
