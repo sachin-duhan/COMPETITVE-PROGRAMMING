8565e0f8202ff08ea31971443dcd683b36034c8c
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-05 07:14:55 +0530

    trying 894

---
+++ LEETCODE/HARD/894. All possible binary tree.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
