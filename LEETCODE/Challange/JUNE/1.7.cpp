7c803c6d152e2056340618699501ce4b7f71cfcb
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-08 13:33:22 +0530

    updating solution

--- LEETCODE/Challange/JUNE/1.7.cpp
+++ LEETCODE/Challange/JUNE/1.7.cpp
@@ -36,6 +36,22 @@ public:
     }
 };
 
+class Solution
+{
+public:
+
+    int change(int amount, vector<int> &coins)
+    {
+        int dp[amount + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0] = 1;
+        for (const auto &c : coins)
+            for (int i = c; i <= amount; ++i)
+                dp[i] += dp[i - c];
+        return dp[amount];
+    }
+};
+
 int main()
 {
     int amount = 0;
