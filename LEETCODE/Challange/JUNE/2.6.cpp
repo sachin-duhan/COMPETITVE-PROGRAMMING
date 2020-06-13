cd866c2cbfa73fd2bbda70e7d52ebabe8a80fe16
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-13 19:55:57 +0530

    trying some solution

--- LEETCODE/Challange/JUNE/2.6.cpp
+++ LEETCODE/Challange/JUNE/2.6.cpp
@@ -11,20 +11,27 @@ public:
         for (int i = 0; i < _size; i++)
         {
             vector<int> temp;
-            temp.push_back(nums[i]);
-            for (int j = i + 1; j < _size; j++)
-            {
-                
-            }
-            if (temp.size() > ans.size()) ans = temp;
+            int num = nums[i];
+            temp.push_back(num);
+            if (nums[i] > 0)
+                for (int j = i + 1; j < _size; j++)
+                    if (nums[j] % num == 0)
+                    {
+                        temp.push_back(nums[j]);
+                        num = nums[j];
+                        nums[j] = -1;
+                    }
+            if (temp.size() > ans.size())
+                ans = temp;
         }
+        return ans;
     }
 };
 
 int main()
 {
     Solution solution;
-    vector<int> v{1, 2, 3, 4, 5, 6, 7, 8}, ans = solution.largestDivisibleSubset(v);
+    vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 18, 32}, ans = solution.largestDivisibleSubset(v);
     for (int a : ans)
         cout << a << " ";
     cout << endl;
