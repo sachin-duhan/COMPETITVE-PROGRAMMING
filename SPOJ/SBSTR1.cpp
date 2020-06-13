6418445e7ffa44d1a5e6d1f3dad296cb5492d3bd
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-13 13:01:44 +0530

    trying bruteforce!

--- SPOJ/SBSTR1.cpp
+++ SPOJ/SBSTR1.cpp
@@ -2,36 +2,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool solve(string s, string t){
+// bool solve(string s, string t){
 
-    vector<int> back(t.length(), 0);
-    int i = 0, j = 0;
-    while (j < s.length())
-    {
-        if (t[i] == t[j])
-        {
-            back[j] = i + 1;
-            i++;
-            j++;
-        }
-        else
-            j++;
-    }
+//     vector<int> back(n, 0);
+//     int i = 0, j = 0;
+//     while (j < m)
+//     {
+//         if (t[i] == t[j])
+//         {
+//             back[j] = i + 1;
+//             i++;
+//             j++;
+//         }
+//         else
+//             j++;
+//     }
 
-    for(i = 0; i < s.length(); i++)
+//     for(i = 0; i < m; i++)
+//     {
+//         while (s[i++] == t[j++])continue;
+//         if (j == i + n - 1)
+//             return true;
+//         else
+//             j = back[j];
+//     }
+//     return false;
+// }
+
+bool solve(string s, string t)
+{
+    int m = s.length(), n = t.length();
+    for (int i = 0; i < m; i++)
     {
-        while (s[i++] == t[j++])continue;
-        if (j == i + t.length() - 1)
-            return true;
-        else
-            j = back[j];
+        int j = 0;
+        while(s[i+j] == t[j]){j++;}
+        if(j == i+n-1) return true;
     }
     return false;
 }
 
 int main()
 {
-    string s = "1010110010", t = "10110";
+    string s = "defabc", t = "abc";
     cout << solve(s, t) << endl;
     return 0;
 }
