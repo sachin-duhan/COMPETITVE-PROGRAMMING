4b9f0085febe54c2d1b232de93d5c481f5c8a9de
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-14 12:03:05 +0530

    solution of diameter of BT

---
+++ LEETCODE/ALL PROBLEMS/662.py
@@ -0,0 +1,22 @@
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+
+        # table contains the first col_index for each level
+        first_col_index_table = {}
+        max_width = 0
+
+        def DFS(node, depth, col_index):
+            nonlocal max_width
+            if node is None:
+                return
+            # if the entry is empty, set the value
+            if depth not in first_col_index_table:
+                first_col_index_table[depth] = col_index
+
+            max_width = max(max_width, col_index - first_col_index_table[depth] + 1)
+
+            # Preorder DFS, with the priority on the left child
+            DFS(node.left, depth+1, 2*col_index)
+            DFS(node.right, depth+1, 2*col_index + 1)
+        DFS(root, 0, 0)
+        return max_width
