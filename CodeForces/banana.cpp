8b531f4d2fa5a5a738857c81482e477f4c938976
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-26 01:53:40 +0530

    kya hi galti krta hu mei!! chi chi

--- CodeForces/banana.cpp
+++ CodeForces/banana.cpp
@@ -8,6 +8,9 @@ int main()
     long long sum = 0;
     for (int i = 1; i <= w; i++)
         sum += i * k;
-    cout << abs(n - sum) << endl;
+    if (sum >= n)
+        cout << sum - n << endl;
+    else
+        cout << 0 << endl;
     return 0;
 }
