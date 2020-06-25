1ae84707e29c23052d2852dae86801c40917ffdf
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-26 02:44:58 +0530

    tried one method but failed!

--- CodeForces/taxi.cpp
+++ CodeForces/taxi.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 int main()
 {
     int n;
@@ -8,16 +8,18 @@ int main()
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    int curr = 0;
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if(arr[i] != -1)
-        }
-        
-    }
+    int ans = 0, i = 0;
+    sort(arr,arr + n);
+    // while(i < n) {
+    //     int curr = 0;
+    //     bool is_counted = false;
+    //     while(curr <= 4 && i < n){
+    //         if(curr + arr[i] > 4) break;
+    //         if(curr + arr[i] < 4) {curr+=arr[i]; i++;}
+    //         else if(curr == 4) {ans++; is_counted = true; break;}
+    //     }
+    //     if(!is_counted) { ans++; i++;}
+    // }
     
     cout << ans << endl;
     return 0;
