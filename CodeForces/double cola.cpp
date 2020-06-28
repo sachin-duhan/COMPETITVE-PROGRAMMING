1ccf78a3c7e2bf30f3d9101a1154d075ab54bbb8
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-29 03:07:55 +0530

    adding some solution

---
+++ CodeForces/double cola.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<string> names{"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+    int n;
+    cin >> n;
+    
+    return 0;
+}
