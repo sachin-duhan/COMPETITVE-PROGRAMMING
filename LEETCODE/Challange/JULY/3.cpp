1d2a34b0e9fcfc2510677d125ce341579116b044
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-07-03 14:54:45 +0530

    adding final solution

--- LEETCODE/Challange/JULY/3.cpp
+++ LEETCODE/Challange/JULY/3.cpp
@@ -36,6 +36,26 @@ public:
     }
 };
 
+class Solution
+{
+public:
+    vector<int> prisonAfterNDays(vector<int> &cells, int N)
+    {
+
+        N = N % 14 == 0 ? 14 : N % 14;
+        vector<int> cells2(8, 0);
+        while (N > 0)
+        {
+            for (int i = 1; i < cells.size() - 1; i++)
+                cells2[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+            cells = cells2;
+            N--;
+        }
+
+        return cells;
+    }
+};
+
 int main()
 {
     vector<int> cells = {1, 0, 0, 1, 0, 0, 1, 0};
