392ebb279d376b3ec1ef644991ca84f6917258de
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-07-08 00:32:16 +0530

    adding final simple solution

--- LEETCODE/Challange/JULY/7.cpp
+++ LEETCODE/Challange/JULY/7.cpp
@@ -2,49 +2,32 @@
 using namespace std;
 class Solution
 {
-    int ans, m, n;
-    int add_to_perimeter(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &visited)
+    int DFS(vector<vector<int>> &grid, int x, int y, vector<vector<bool>> &visited)
     {
-        int perimeter = 0;
-        if (grid[i][j] == 0)
-            return perimeter;
-
-        perimeter += (i > 0 && grid[i - 1][j] == 0) ? 1 : 0;
-        perimeter += (j > 0 && grid[i][j - 1] == 0) ? 1 : 0;
-        perimeter += (i < m - 1 && grid[i + 1][j] == 0) ? 1 : 0;
-        perimeter += (i < n && grid[i][j + 1] == 0) ? 1 : 0;
-
-        return perimeter;
-    }
-
-    void dfs(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &visited)
-    {
-        if (grid[i][j] == 0)
-            return;
-
-        ans += add_to_perimeter(grid, i, j, visited);
-        visited[i][j] = 0;
-
-        if(i>0)dfs(grid,i-1,j,visited);
-        if(j>0)dfs(grid,i,j-1,visited);
-        if(i<m-1)dfs(grid,i+1,j,visited);
-        if(j<n-1)dfs(grid,i,j+1,visited);
+        if (x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size() || !grid[x][y])
+            return 1;
+        if (visited[x][y])
+            return 0;
+        visited[x][y] = true;
+        int a = DFS(grid, x + 1, y, visited);
+        a += DFS(grid, x - 1, y, visited);
+        a += DFS(grid, x, y + 1, visited);
+        a += DFS(grid, x, y - 1, visited);
+        return a;
     }
 
 public:
-    int islandPerimeter(vector<vector<int>> grid)
+    int islandPerimeter(vector<vector<int>> &grid)
     {
-        ans = 0;
-        m = grid.size();
-        n = m > 0 ? grid[0].size() : 0;
-        auto visited = grid;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (grid[i][j] == 1 && visited[i][j] == 1)
-                    dfs(grid, i, j, visited);
-        return ans;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[i].size(); j++)
+                if (grid[i][j])
+                    return DFS(grid, i, j, visited);
+        return 0;
     }
 };
+
 int main()
 {
 }
