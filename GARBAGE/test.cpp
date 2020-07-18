1fd298cabaafd3df7a24f66f15ba5432faff1015
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-07-18 11:57:02 +0530

    kuch b

---
+++ GARBAGE/test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+int main()
+{   
+    cout << factorial(5) << endl;
+    return 0;
+}
