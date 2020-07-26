95d9dd0713173c4ada27d82d3c5bef18a1655ee8
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-07-27 03:15:27 +0530

    update

--- LEETCODE/ALL PROBLEMS/1314.cpp
+++ LEETCODE/ALL PROBLEMS/1314.cpp
@@ -42,6 +42,41 @@ public:
     }
 };
 
+// prefix sum solution
+class Solution
+{
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>> &mat, int K)
+    {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<vector<int>> sum(rows + 1, vector<int>(cols + 1, 0));
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] - sum[i][j] + mat[i][j];
+
+        vector<vector<int>> ans(rows, vector<int>(cols, 0));
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int ROW1 = max(0, i - K);
+                int ROW2 = min(rows - 1, i + K);
+                int COL1 = max(0, j - K);
+                int COL2 = min(cols - 1, j + K);
+                ans[i][j] = sum[ROW2 + 1][COL2 + 1];
+                if (COL1 - 1 >= 0)
+                    ans[i][j] -= sum[ROW2 + 1][COL1];
+                if (ROW1 - 1 >= 0)
+                    ans[i][j] -= sum[ROW1][COL2 + 1];
+                if (COL1 - 1 >= 0 && ROW1 - 1 >= 0)
+                    ans[i][j] += sum[ROW1][COL1];
+            }
+        }
+        return ans;
+    }
+};
+
 int main(){
 return 0;
 }
