65f9dd8e580f2cba1faef246ed68ecd4c22d268f
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-07-27 17:52:31 +0530

    udpate

--- LEETCODE/Challange/JULY/4.4.cpp
+++ LEETCODE/Challange/JULY/4.4.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution
 {
 public:
     int findMin(vector<int> &nums)
     {
         return *min_element(nums.begin(), nums.end());
+    }
+};
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int ans = INT_MAX, i=0,j=nums.size()-1;
+        while(i <= j){
+            ans = min(nums[i],ans);
+            ans = min(nums[j],ans);
+            i++;j--;
+        }
+        return ans;
     }
 };
