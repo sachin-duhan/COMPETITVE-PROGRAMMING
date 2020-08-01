f88f3247e1b35d3ef5816cae3848d70178ad3468
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-08-02 00:10:31 +0530

    updating as per new test

--- INTERVIEW/codenation/1.cpp
+++ INTERVIEW/codenation/1.cpp
@@ -1,43 +1,42 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int solve(vector<pair<int,int>> adj, vector<int> ai, int a, int x)
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'stringReduction' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. STRING s
+ */
+
+string stringReduction(int k, string s)
 {
-    int ans = 0;
-    
-    return ans % 998244353;
+    int n = s.length();
+    if (k == 0)
+        return s;
+    if (k >= n)
+        return "";
+
+    map<char, int> m;
+    for (int i = 0; i < n; i++)
+        m[s[i]] = i;
+    auto itr = m.begin();
+    while (k-- && itr != m.end())
+    {
+        if(itr->second == 0) itr++;
+        s.erase(itr->second);
+        itr->second--;    
+    }
+    return s;
 }
 
 int main()
 {
-    int n, m, x;
-    cin >> n >> m >> x;
-    vector<pair<int, int>> adj(n, vector<bool>(n, false));
-    for (int i = 0; i < n - 1; i++)
-    {
-        int row, col;
-        cin >> row >> col;
-        adj.push_back({row, col});
-    }
-    sort(adj.begin(), adj.end(), [](auto a, auto b) { return a.first == b.first ? a.second < b.second });
-    vector<int> ai(n, 0);
-    for (int i = 0; i < n; i++) cin >> ai[i];
-    for (int i = 0; i < m; i++)
-    {
-        int q;
-        cin >> q;
-        if (q == 1)
-        {
-            int index, val;
-            cin >> index >> val;
-            ai[index] = val;
-        }
-        else
-        {
-            int a;
-            cin >> a;
-            cout << solve(adj, ai, a, x) << endl;
-        }
-    }
+    cout << stringReduction(2, "zyxedcba") << endl;
     return 0;
 }
