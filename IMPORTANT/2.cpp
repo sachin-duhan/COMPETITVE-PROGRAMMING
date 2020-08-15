5b4fd75aca4042329898289179cdc478ebccbfa7
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-08-16 03:18:01 +0530

    adding sliding window

--- IMPORTANT/2.cpp
+++ IMPORTANT/2.cpp
@@ -20,6 +20,30 @@ pair<int, int> solve(vector<int> nums, int sum)
     return {-1, -1};
 }
 
+pair<int, int> sliding_window_solution(vector<int> nums, int sum)
+{
+    int n = nums.size();
+    if (!n)
+        return {-1, -1};
+    int i = 0, j = 0, curr = nums[0];
+    while (curr != sum && j < n - 1)
+    {
+        if (curr > sum)
+        {
+            curr -= nums[i];
+            i++;
+        }
+        else if (curr < sum)
+        {
+            j++;
+            curr += nums[j];
+        }
+
+        if (curr == sum)
+            return {i, j};
+    }
+    return {-1, -1};
+}
 int main()
 {
 }
