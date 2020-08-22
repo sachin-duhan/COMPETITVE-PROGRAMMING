94d41e5ffe6897a1463a98629fe17e8cd3060745
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-08-23 03:47:30 +0530

    update

--- _LEETCODE/PROBLEMS/min path in triangle.cpp
+++ _LEETCODE/PROBLEMS/min path in triangle.cpp
@@ -25,3 +25,19 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    int minimumTotal(vector<vector<int>> &v)
+    {
+        int i, j;
+        int n = v.size();
+        if (n == 0)
+            return 0;
+        for (i = n - 2; i >= 0; i--)
+            for (j = 0; j < v[i].size(); j++)
+                v[i][j] = min(v[i][j] + v[i + 1][j], v[i][j] + v[i + 1][j + 1]);
+        return v[0][0];
+    }
+};
