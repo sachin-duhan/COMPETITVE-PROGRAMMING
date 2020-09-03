bbb3a2ec8573c678cf6217d87bfd09e7db770bae
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-04 02:30:41 +0530

    adding faster solution

--- Challange/SEPTEMBER/1.3.cpp
+++ Challange/SEPTEMBER/1.3.cpp
@@ -38,3 +38,24 @@ int main()
     cout << (solve.repeatedSubstringPattern(input) == true ? "TRUE" : "FALSE") << endl;
     return 0;
 }
+
+// another solution
+class Solution
+{
+public:
+    bool repeatedSubstringPattern(string str)
+    {
+        int i = 1, j = 0, n = str.size();
+        vector<int> dp(n + 1, 0);
+        while (i < n)
+        {
+            if (str[i] == str[j])
+                dp[++i] = ++j;
+            else if (j == 0)
+                i++;
+            else
+                j = dp[j];
+        }
+        return dp[n] && dp[n] % (n - dp[n]) == 0;
+    }
+};
