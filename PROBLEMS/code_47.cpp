727417dac6ed87f325a90ae649364c5610783103
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-09 04:42:59 +0530

    bakwas question

---
+++ PROBLEMS/code_47.cpp
@@ -0,0 +1,16 @@
+// find all sub array for the given array!!
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> subarra(vector<int> nums)
+{
+    vector<vector<int>> ans;
+    vector<int> temp;
+        
+    return ans;
+}
+int main()
+{
+    vector<int> nums = {10, 20, 30, 50, 40};
+    return 0;
+}
