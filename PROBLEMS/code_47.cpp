b8c8bcc6b12b71bf6963e9edf3941cae903a6be9
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-09 16:17:05 +0530

    updating  solution

--- PROBLEMS/code_47.cpp
+++ PROBLEMS/code_47.cpp
@@ -1,16 +1,30 @@
-// find all sub array for the given array!!
-
 #include <bits/stdc++.h>
 using namespace std;
+
 vector<vector<int>> subarra(vector<int> nums)
 {
     vector<vector<int>> ans;
-    vector<int> temp;
-        
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+
+        vector<int> temp;
+        temp.push_back(nums[i]);
+        for (size_t j = i + 1; j + 1 < nums.size(); j++)
+            temp.push_back(nums[j]);
+        ans.push_back(temp);
+    }
     return ans;
 }
+
 int main()
 {
     vector<int> nums = {10, 20, 30, 50, 40};
+    auto ans = subarra(nums);
+    for (auto arr : ans)
+    {
+        for (int a : arr)
+            cout << a << " ";
+        cout << endl;
+    }
     return 0;
 }
