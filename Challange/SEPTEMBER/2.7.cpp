dce7fd4207ae854ca2d51d824ec68746231313c8
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-15 04:10:51 +0530

    update

--- Challange/SEPTEMBER/2.7.cpp
+++ Challange/SEPTEMBER/2.7.cpp
@@ -32,6 +32,19 @@ public:
     }
 };
 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int sum1 = 0,sum2 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(!(i&1))sum1=max(sum1+nums[i],sum2);
+            else sum2 = max(sum2+nums[i],sum1);
+        }
+        return max(sum1,sum2);
+    }
+};
+
+
 
 /*===== TREENODE CODE =====
 struct TreeNode {
