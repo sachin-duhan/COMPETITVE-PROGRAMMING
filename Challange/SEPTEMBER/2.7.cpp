8d1e20ab221e7ddc0dc2c21460cc37c5fa19feae
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-15 04:04:47 +0530

    updating solution

---
+++ Challange/SEPTEMBER/2.7.cpp
@@ -0,0 +1,44 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int ans =0 ;
+    void rob_helper(vector<int>houses,int index =0, int curr=0){
+        ans = max(ans,curr);
+        if(index >= houses.size())return;
+        rob_helper(houses,index+1,curr);
+        rob_helper(houses,index+2,curr+houses[index]);
+    }
+public:
+    int rob(vector<int>& nums) {
+        rob_helper(nums);
+        return ans;
+    }
+};
+
+
+/*===== TREENODE CODE =====
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode * left, TreeNode * right): val(x), left(left), right(right) {}
+};
+*/
+
+/* == GRAPH NODE TEMPLATE CODE ===
+struct Node {
+    int val;
+    vector < Node * > neighbors;
+    Node(): val(0), neighbors({}) {};
+    Node(int x): val(x), neighbors({}) {};
+    Node(int x, vector < Node * > neighbors): val(x), neighbors(neighbors) {};
+};
+*/
+
+int main(){
+    return 0;
+}
