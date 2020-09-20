a94ed6dc9cda02b1543eb5f440c2d7d4c281870e
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-20 22:43:13 +0530

    removed comments

--- PEPCODING/OOPS/oops-test.cpp
+++ PEPCODING/OOPS/oops-test.cpp
@@ -1,27 +1,5 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-/*===== TREENODE CODE =====
-struct TreeNode {
-    int val;
-    TreeNode * left;
-    TreeNode * right;
-    TreeNode(): val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode * left, TreeNode * right): val(x), left(left), right(right) {}
-};
-*/
-
-/* == GRAPH NODE TEMPLATE CODE ===
-struct Node {
-    int val;
-    vector < Node * > neighbors;
-    Node(): val(0), neighbors({}) {};
-    Node(int x): val(x), neighbors({}) {};
-    Node(int x, vector < Node * > neighbors): val(x), neighbors(neighbors) {};
-};
-*/
 
 class Person
 {
