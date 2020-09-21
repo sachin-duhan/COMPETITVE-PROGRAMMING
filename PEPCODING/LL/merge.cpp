21f2051e4bed04972a5628bafe70eb1dd5c3f5bb
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-21 18:00:25 +0530

    adding another solution & reodering things

---
+++ PEPCODING/LL/merge.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int val;
+    Node *next;
+    Node() : val(0), next(NULL){};
+    Node(int val) : val(val), next(NULL){};
+};
+
+Node *insert(vector<int> nums)
+{
+    Node *head = new Node(nums[0]), *temp = head;
+    int i = 1;
+    while (temp && i < nums.size())
+    {
+        Node *node = new Node(nums[i++]);
+        temp->next = node;
+        temp = temp->next;
+    }
+    return head;
+}
+
+void print(const Node *head)
+{
+    if (!head)
+        return;
+    print(head->next);
+    cout << head->val << " ";
+}
+
+int main()
+{
+    Node *head = insert({1, 2, 3, 4, 5, 6, 7, 8});
+    // print(head);
+    kth_element(head, 3);
+    return 0;
+}
