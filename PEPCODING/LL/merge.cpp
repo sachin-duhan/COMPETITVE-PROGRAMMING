4421dcf80c1e78b0911666e65c3b8b1fa19b8a24
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-21 18:26:44 +0530

    merge two sorted linked list

--- PEPCODING/LL/merge.cpp
+++ PEPCODING/LL/merge.cpp
@@ -30,10 +30,54 @@ void print(const Node *head)
     cout << head->val << " ";
 }
 
+Node *merge(Node *h1, Node *h2)
+{
+    if (!h2) return h1;
+    if (!h1) return h2;
+    Node *head = new Node(), *dummyHead = head, *t1 = h1, *t2 = h2;
+
+    while (t1 && t2)
+    {
+        if (t1->val < t2->val)
+        {
+            head->next = new Node(t1->val);
+            head = head->next;
+            t1 = t1->next;
+        }
+        else
+        {
+            head->next = new Node(t2->val);
+            head = head->next;
+            t2 = t2->next;
+        }
+    }
+
+    if (t1)
+    {
+        while (t1)
+        {
+            head->next = new Node(t1->val);
+            head = head->next;
+            t1 = t1->next;
+        }
+    }
+    else if (t2)
+    {
+        while (t2)
+        {
+            head->next = new Node(t2->val);
+            head = head->next;
+            t2 = t2->next;
+        }
+    }
+    return dummyHead->next;
+}
+
 int main()
 {
-    Node *head = insert({1, 2, 3, 4, 5, 6, 7, 8});
-    // print(head);
-    kth_element(head, 3);
+    Node *head = insert({3, 4, 5, 6, 40});
+    Node *head2 = insert({1, 2, 7, 8, 9, 34});
+    Node *temp = merge(head, head2);
+    print(temp);
     return 0;
 }
