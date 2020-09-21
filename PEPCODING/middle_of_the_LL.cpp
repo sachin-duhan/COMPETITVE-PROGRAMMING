b9adf54af97bb32c9ec4cebedd6908c8a9ff3862
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-21 16:40:18 +0530

    middle of the linked list

---
+++ PEPCODING/middle_of_the_LL.cpp
@@ -0,0 +1,63 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int val;
+    Node *next;
+    Node() : val(0), next(NULL){};
+    Node(int val) : val(val), next(NULL){};
+};
+
+Node *insert(vector<int> nums)
+{
+    Node *head = new Node(nums[0]), *temp = head;
+    int i = 1;
+    while (temp && i < nums.size())
+    {
+        Node *node = new Node(nums[i++]);
+        temp->next = node;
+        temp = temp->next;
+    }
+    return head;
+}
+
+void print(const Node *head)
+{
+    if (!head)
+        return;
+    print(head->next);
+    cout << head->val << " ";
+}
+
+int kth_element(Node *head, int k)
+{
+    if (!head)
+        return 0;
+    int temp = kth_element(head->next, k) + 1;
+    if (temp == k)
+        cout << head->val << endl;
+    return temp;
+}
+
+int middle(Node *head)
+{
+    Node *slow = head, *fast = head;
+    while (fast)
+    {
+        fast = fast->next;
+        if (!fast)
+            return slow->val;
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return slow->val;
+}
+
+int main()
+{
+    Node *head = insert({1, 2, 3, 4, 5, 6, 7, 8});
+    cout << middle(head) << endl;
+    return 0;
+}
