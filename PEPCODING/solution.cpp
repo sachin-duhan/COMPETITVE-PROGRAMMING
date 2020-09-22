d095f332f7ecaebe2f4807043e099f9da618f80f
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-23 01:33:59 +0530

    updating solution

--- PEPCODING/solution.cpp
+++ PEPCODING/solution.cpp
@@ -29,3 +29,38 @@ public:
         return ans;
     }
 };
+
+
+class Solution2
+{
+public:
+    int solve(vector<int> &nums, int k)
+    {
+        // Write your code here
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ans = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            int l = i + 1, h = n - 1;
+            int curr = nums[i];
+            while (l < h)
+            {
+                if (abs(nums[l] + nums[h] + curr - k) < ans)
+                {
+                    ans = abs(nums[l] + nums[h] + curr - k);
+                }
+                else if (nums[l] + nums[h] + curr > k)
+                {
+                    h--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
