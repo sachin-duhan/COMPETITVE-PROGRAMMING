10fc028b23367ce5edba233744fa7942805a6115
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-22 23:44:13 +0530

    updating slight better solution

--- Challange/SEPTEMBER/4.1.cpp
+++ Challange/SEPTEMBER/4.1.cpp
@@ -2,6 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+TIME - O(N^2)
+SPACE - O(1)
+*/
 vector<int> majorityElement(vector<int> nums)
 {
     vector<int> ans;
@@ -23,10 +27,26 @@ vector<int> majorityElement(vector<int> nums)
     return ans;
 }
 
+vector<int> majorityElement2(vector<int> nums)
+{
+    sort(nums.begin(), nums.end()); // O(NlogN)
+    vector<int> ans;
+    int i;
+    for(i =0; i < nums.size(); i++){
+        int curr_number = nums[i];
+        int j = i, frequency = 0;
+        while(nums[j++] == curr_number) frequency++;
+        if(frequency > (nums.size()/3)) ans.push_back(curr_number);
+        i = j;
+    }
+    return ans;
+}
+
+
 int main()
 {
     vector<int> val = {1, 1, 1, 2, 2, 3, 3, 3};
-    auto tmp = majorityElement(val);
+    auto tmp = majorityElement2(val);
     for(auto a : tmp) cout << a << " ";
     cout << endl;
     return 0;
