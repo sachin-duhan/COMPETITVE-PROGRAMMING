4c24fd9eec796249879772437e2957227d95b8f7
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-26 04:24:11 +0530

    update

---
+++ PEPCODING/GTree/mirrorSimilar.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    vector<TreeNode *> children;
+    TreeNode() : val(-1), children({}){};
+    TreeNode(int x) : val(x), children({}){};
+    TreeNode(int x, vector<TreeNode *> children) : val(x), children(children){};
+};
+
+class Generic_Tree
+{
+public:
+    TreeNode *root;
+    Generic_Tree(vector<int> tree)
+    {
+        stack<TreeNode *> s;
+        for (auto val : tree)
+        {
+            if (val == -1)
+                s.pop();
+            else
+            {
+                TreeNode *temp = new TreeNode(val);
+                if (s.empty())
+                    this->root = temp;
+                else
+                    s.top()->children.push_back(temp);
+                s.push(temp);
+            }
+        }
+    }
+};
+
+void print(TreeNode *root)
+{
+    if (!root)
+        return;
+    string level = to_string(root->val) + " -> ";
+    for (auto child : root->children)
+        level += to_string(child->val) + ", ";
+    cout << level << endl;
+    for (auto child : root->children)
+        print(child);
+}
+
+bool isSimilar(TreeNode *root1, TreeNode *root2)
+{
+    if (root1->children.size() != root2->children.size())
+        return false;
+    int n = root1->children.size();
+    for (int i = 0; i < n; i++)
+    {
+        auto isSubTreeSimilar = isSimilar(root1->children[i], root2->children[n - i - 1]);
+        if (!isSubTreeSimilar)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    vector<int> tree = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 170, -1, -1, -1};
+    Generic_Tree mtree(tree);
+    return 0;
+}
