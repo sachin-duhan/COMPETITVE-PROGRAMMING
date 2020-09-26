34ee89f661697660d00aa8bdcab6b29475c4cb9d
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-26 15:30:31 +0530

    random

---
+++ PEPCODING/GTree/isSymmetric.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    vector<TreeNode *> children;
+    TreeNode() : val(-1), children({}){};
+    TreeNode(int x) : val(x), children({}){};
+    TreeNode(int x, vector<TreeNode *> children) : val(x), children(children){};
+};
+
+class Generic_Tree
+{
+    bool _isSymmtric_helper(TreeNode *root1, TreeNode *root2){
+
+    }
+
+public:
+    TreeNode *root;
+    Generic_Tree(vector<int> tree)
+    {
+        stack<TreeNode *> s;
+        for (auto val : tree)
+        {
+            if (val == -1)
+                s.pop();
+            else
+            {
+                TreeNode *temp = new TreeNode(val);
+                if (s.empty())
+                    this->root = temp;
+                else
+                    s.top()->children.push_back(temp);
+                s.push(temp);
+            }
+        }
+    }
+
+    bool isSymmtic(){
+        if(!this->root) return false;
+        int i =0, j = this->root->children.size()-1;
+        // while()
+    }
+};
+
+void print(TreeNode *root)
+{
+    if (!root)
+        return;
+    string level = to_string(root->val) + " -> ";
+    for (auto child : root->children)
+        level += to_string(child->val) + ", ";
+    cout << level << endl;
+    for (auto child : root->children)
+        print(child);
+}
+
+int main()
+{
+    vector<int> tree = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 170, -1, -1, -1};
+    Generic_Tree mtree(tree);
+    print(mtree.root);
+    return 0;
+}
