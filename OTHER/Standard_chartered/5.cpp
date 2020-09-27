dd94d328beb6860c0815b8ef42cd59a5b51fc843
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-09-27 16:04:41 +0530

    shifting and swapping numbers

---
+++ OTHER/Standard_chartered/5.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// moving all negetive numbers to beganing!!
+void solve(int arr[], int n)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            if (i != j)
+                swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}
