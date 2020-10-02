9e822731861349b9595fb595c932f1daa9b4aca0
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-10-02 20:49:29 +0530

    added 797

---
+++ _LEETCODE/GRAPH/797.cpp
@@ -0,0 +1,29 @@
+// https : //leetcode.com/problems/all-paths-from-source-to-target/
+
+#include<bits/stdc++.h>
+        using namespace std;
+
+class Solution
+{
+    void dfs(vector<vector<int>> &graph, vector<vector<int>> &result, vector<int> &path, int u)
+    {
+        path.push_back(u);
+        if (u == graph.size() - 1)
+            result.push_back(path);
+        else
+        {
+            for (auto v : graph[u])
+                dfs(graph, result, path, v);
+        }
+        path.pop_back();
+    }
+
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>> &graph)
+    {
+        vector<vector<int>> result;
+        vector<int> path;
+        dfs(graph, result, path, 0);
+        return result;
+    }
+};
