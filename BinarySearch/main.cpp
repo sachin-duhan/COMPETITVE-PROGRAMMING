5881b3e9c6ec842c40b84e2abdd8d1110cae4757
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-10-29 15:23:28 +0530

    updating code

---
+++ BinarySearch/main.cpp
@@ -0,0 +1,43 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+/*===== TREENODE CODE =====
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode * left, TreeNode * right): val(x), left(left), right(right) {}
+};
+*/
+
+/* == GRAPH NODE TEMPLATE CODE ===
+struct Node {
+    int val;
+    vector < Node * > neighbors;
+    Node(): val(0), neighbors({}) {};
+    Node(int x): val(x), neighbors({}) {};
+    Node(int x, vector < Node * > neighbors): val(x), neighbors(neighbors) {};
+};
+*/
+
+int main(){
+	cout << phi(29) << endl;
+    	return 0;
+}
