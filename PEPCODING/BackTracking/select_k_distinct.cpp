0c1b59d912878678bf1c9d3aab5d33ecd5404f29
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-11-02 02:27:38 +0530

    updarting ans

--- PEPCODING/BackTracking/select_k_distinct.cpp
+++ PEPCODING/BackTracking/select_k_distinct.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ans_len 3
 void helper(string str, int i, string ans)
 {
     if (i >= str.length())
     {
-        if (ans.length() == 3)
+        if (ans.length() == ans_len)
             cout << ans << endl;
         return;
     }
-    helper(str, i + 1, ans);
     helper(str, i + 1, ans + str[i]);
+    helper(str, i + 1, ans);
 }
 
 void solve(string str)
