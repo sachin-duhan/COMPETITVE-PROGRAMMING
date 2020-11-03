32ff47e6cd86d772445d5068a644361df8f773d5
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-11-04 04:05:00 +0530

    updating solution

---
+++ BinarySearch/stairs_dp.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*===== TREENODE CODE =====
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode * left, TreeNode * right): val(x), left(left), right(right) {}
+};
+*/
+int solve(int n)
+{
+    const int mod = (pow(10, 9)) + 7;
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+    return dp[n] % mod;
+}
+
+/* == GRAPH NODE TEMPLATE CODE ===
+struct Node {
+    int val;
+    vector < Node * > neighbors;
+    Node(): val(0), neighbors({}) {};
+    Node(int x): val(x), neighbors({}) {};
+    Node(int x, vector < Node * > neighbors): val(x), neighbors(neighbors) {};
+};
+*/
+
+int main()
+{
+    return 0;
+}
