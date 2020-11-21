3602cae516f78f6c7fa26a3bafc7737dd0617b01
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-11-21 17:33:38 +0530

    updating solution

---
+++ A2OJ/questions/alternateChars.cpp.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int alternatingCharacters(string s) {
+    int ans = 0, n = s.length(), i =0;
+    while(i<n){        
+        char c = s[i++];
+        while(i<n && s[i]==c){
+            ans++;
+            i++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = alternatingCharacters(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*===== TREENODE CODE =====
+struct TreeNode {
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode * left, TreeNode * right): val(x), left(left), right(right) {}
+};
+*/
+
+/* == GRAPH NODE TEMPLATE CODE ===
+struct Node {
+    int val;
+    vector < Node * > neighbors;
+    Node(): val(0), neighbors({}) {};
+    Node(int x): val(x), neighbors({}) {};
+    Node(int x, vector < Node * > neighbors): val(x), neighbors(neighbors) {};
+};
+*/
+
+int main(){
+    return 0;
+}
